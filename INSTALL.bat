7c807820117ad73c2386f5c746fe5a1b416dcb7e
NKStudiosCO <nikobikoliko@gmail.com>
2016-11-22 21:02:23 -0800

    Add files via upload

---
+++ INSTALL.bat
@@ -0,0 +1,61 @@
+@echo off
+
+echo --------------------------------------------------------------------------------
+%name%
+echo                  _      ___      _______ .   .          __
+echo   \    /   I    I \    /     I      I     \ /             \    
+echo    \  /    I    I  I   I     I      I      I    	--I 
+echo     \/     I    I_/    \___  I      I      I		__/
+%name%
+%name%
+echo ____________
+echo [          ]
+echo [ [] [] [] ]____________
+echo [          ][          ]
+echo [ [] [] [] ][ [] [] [] ]                      
+echo [          ][          ]                                
+echo [ [] [] [] ][ [] [] [] ]            ____________                
+echo [          ][          ]            [          ]                             
+echo [ [] [] [] ][ [] [] [] ]            [ [] [] [] ]_____________________   
+echo [          ][          ]            [          ][                   ]
+echo [ [] [] [] ][ [] [] [] ]____________[ [] [] [] ][ [] [] [] [] [] [] ]     
+echo [          ][          ][          ][          ][                   ]  
+echo [ [] [] [] ][ [] [] [] ][ [] [] [] ][ [] [] [] ][ [] [] [] [] [] [] ]      
+echo [          ][          ][          ][          ][                   ]
+echo [ [] [] [] ][ [] [] [] ][ [] [] [] ][ [] [] [] ][ [] [] [] [] [] [] ]      
+echo [_________ ][_________ ][_________ ][_________ ][__________________ ]
+echo ================================================================================
+echo (c) NK Studios
+echo A TDK processer game
+echo (c) 2014
+echo .
+echo To register your game, type B64H7%name%
+set /p name=
+echo Product Registered! Now press the "Enter" key to install                 %name%
+set /p name=
+echo Compiling . . .                                    %name%
+set /p name=
+echo Inserting 3D . . .                                %name%
+set /p name=
+echo Locating . . .                                        %name%
+set /p name=
+echo ARCH.3D . . .                                      %name%
+set /p name=
+echo Hex.bin . . .                                         %name%
+set /p name=
+echo MC.js . . .                                             %name%
+set /p name=
+echo TUT.bat . . .                                         %name%
+set /p name=
+echo JavaPro.js . . .                                     %name%
+set /p name=
+echo Generating . . .                                   %name%
+set /p name=
+echo Loading . . .                                        %name%
+set /p name=
+echo Now please copy and pste VidCity 3 onto desktop . . .%name%
+echo .
+echo ================================================================================
+echo To close the installer, hit Ctrl+C
+set /p name= 
+
